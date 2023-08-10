536cc69cfdd6c77565b3f4a61399e096dbb323db
SeoJaeMin619 <emiya1663@naver.com>
2023-08-10 10:43:39 +0900

    T.T

--- tooththth/Assets/cripts/gamemanager.cs
+++ tooththth/Assets/cripts/gamemanager.cs
@@ -13,18 +13,21 @@ public class gamemanager : MonoBehaviour
 
     void Start()
     {
+        int[] rtans = { 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7 };
+
+        rtans = rtans.OrderBy(item => Random.Range(-1.0f, 1.0f)).ToArray();
+
         for (int i = 0; i < 16; i++)
         {
-            int[] rtans = { 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7 };
-            rtans = rtans.OrderBy(item => Random.Range(-1.0f, 1.0f)).ToArray();
-
-            GameObject newcard = Instantiate(card);
-            newcard.transform.parent = GameObject.Find("cards").transform;
+            GameObject newCard = Instantiate(card);
+            newCard.transform.parent = GameObject.Find("cards").transform;
 
             float x = (i / 4) * 1.4f - 2.1f;
             float y = (i % 4) * 1.4f - 3.0f;
-            newcard.transform.position = new Vector3(x, y, 0);
-            Debug.Log(rtans[i]);
+            newCard.transform.position = new Vector3(x, y, 0);
+
+            string rtanName = "rtan" + rtans[i].ToString();
+            newCard.transform.Find("front").GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>(rtanName);
         }
     }
 
