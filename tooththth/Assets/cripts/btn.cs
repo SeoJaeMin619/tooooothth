9361ecef3e7dfafff756ea0e13198d8c91679555
SeoJaeMin619 <emiya1663@naver.com>
2023-08-11 09:59:29 +0900

    찐막

---
+++ tooththth/Assets/cripts/btn.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class startbtn : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void gamestart()
+    {
+        Time.timeScale = 1.0f;
+        SceneManager.LoadScene("mainscene");
+    }
+}
